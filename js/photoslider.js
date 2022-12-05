1c1948effcdb7d7a70d2a3604f7e4bc892d7e77a
Timur <100406268+Timik232@users.noreply.github.com>
2022-12-05 16:20:53 +0300

    almost done

--- js/photoslider.js
+++ js/photoslider.js
@@ -6,6 +6,7 @@
 	Slideshow.prototype = {
 		init: function() {
 			this.wrapper = this.el.querySelector(".slider-wrapper");
+			this.links = this.el.querySelectorAll("#slider-nav button");
 			this.slides = this.el.querySelectorAll(".slide");
 			this.previous = this.el.querySelector(".slider-previous");
 			this.next = this.el.querySelector(".slider-next");
@@ -20,7 +21,11 @@
 		actions: function() {
 			var self = this;
 			self.next.addEventListener("click", function() {
+				let cur = document.querySelector('#slider-nav button.current')
+				cur.classList.remove('current');
 				self.index++;
+				cur = document.querySelector(`[data-slide="${self.index}"]`);
+				cur.classList.add('current');
 				self.previous.style.display = "block";
 				if (self.index == self.total - 1) {
 					self.index = self.total - 1;
@@ -29,7 +34,11 @@
 				self._slideTo(self.index);
 			}, false);
 			self.previous.addEventListener("click", function() {
+				let cur = document.querySelector('#slider-nav button.current')
+				cur.classList.remove('current');
 				self.index--;
+				cur = document.querySelector(`[data-slide="${self.index}"]`);
+				cur.classList.add('current');
 				self.next.style.display = "block";
 				if (self.index == 0) {
 					self.index = 0;
@@ -37,6 +46,29 @@
 				}
 				self._slideTo(self.index);
 			}, false);
+			for (let i = 0; i < self.links.length; i++){
+				self.links[i].addEventListener("click", function() {
+					let cur = document.querySelector('#slider-nav button.current')
+					cur.classList.remove('current');
+					self.links[i].classList.add('current');
+					self.index = i;
+					if (self.index == self.total - 1) {
+						self.index = self.total - 1;
+						self.next.style.display = "none";
+					}
+					else {
+						self.next.style.display = "block";
+					}
+					if (self.index == 0) {
+						self.index = 0;
+						self.previous.style.display = "none";
+					}
+					else {
+						self.previous.style.display = "block";
+					}
+					self._slideTo(self.index);
+				});
+			}
 		}
 	};
 	document.addEventListener("DOMContentLoaded", function() {
